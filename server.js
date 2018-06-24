6e8978de305995ce391d5eefdb7b362a6d512d43
allan <allanmleite@hotmail.com>
2018-06-24 00:57:31 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-app.use(express.static('/dist'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('/dist/srm-client'));
+const path = require('path');
+app.use(express.static('./dist/srm-client'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/srm-client/index.html'));
 });
-
 app.listen(process.env.PORT || 8080);
